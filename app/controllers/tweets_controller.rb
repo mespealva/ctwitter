f037cf0b46ca05d7ae9d43a6552afb5ff225cb9e
maria alvarez <mar.alvareg@gmail.com>
2020-10-03 11:42:07 -0300

    eliminar search

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -62,10 +62,6 @@ class TweetsController < ApplicationController
     end
    end 
 
-  #  def search
-  #   @tweets = Tweet.where("content LIKE ?", "%#{:search}%")
-  #  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tweet
