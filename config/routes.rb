73a4c208daf05663acbed9451f2cd50fea9a21aa
maria alvarez <mar.alvareg@gmail.com>
2020-10-08 08:20:44 -0300

    unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,15 @@ Rails.application.routes.draw do
   devise_for :users, controllers:{
     registrations: 'users/registrations'
   }
-  post 'follow/:user_id', to:'users#follow', as: 'follow'
+  
   resources :tweets, except:[:index, :edit, :update]
   get 'api/news', to:'api#news'
   post 'api/new', to:'api#new'
   get 'api/:fecha1/:fecha2', to:'api#fecha'
   get 'user/:id', to:'users#profile', as: 'profile'
   get 'retweet/:id', to: 'tweets#new_retweet', as: 'retweet' 
+  post 'follow/:user_id', to:'users#follow', as: 'follow'
+  post 'unfollow/:user_id', to:'users#unfollow', as: 'unfollow'
   post 'retweet/:id', to: 'tweets#retweet'
   post 'likes/:id', to: 'tweets#likes', as: 'likes'
 
